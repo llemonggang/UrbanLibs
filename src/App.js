7b5f9c35ff97bba5f928fc9f5fde6fbb29ed1b9e
Jeanette Adelson <jeni.jeanette@gmail.com>
2016-09-19 10:02:24 -0500

    added components

--- src/App.js
+++ src/App.js
@@ -2,17 +2,15 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Sentence from './components/sentence';
+import Words from './components/words';
+
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+
       </div>
     );
   }
---
+++ src/components/words.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+
+class Words extends Component {
+  render () {
+
+  }
+
+}
+export default Words;
